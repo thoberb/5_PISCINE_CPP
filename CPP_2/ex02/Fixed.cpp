f32c750d4b05a8bbdc8b6e0bb68a8114d0c4aa53
Blandine BERTHOD <bberthod@paul-f5Ar3s9.clusters.42paris.fr>
2024-02-20 17:56:20 +0100

    suite

--- CPP_2/ex02/Fixed.cpp
+++ CPP_2/ex02/Fixed.cpp
@@ -3,10 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: blandineberthod <blandineberthod@studen    +#+  +:+       +#+        */
+/*   By: bberthod <bberthod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 14:58:57 by blandineber       #+#    #+#             */
-/*   Updated: 2024/02/16 14:58:58 by blandineber      ###   ########.fr       */
+/*   Updated: 2024/02/20 17:55:34 by bberthod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	_valeur = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int nombre);
+Fixed(const float nombre);
+
+Fixed::Fixed(const Fixed& fixed)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fixed;
+}
+
+Fixed& Fixed::operator=(const Fixed& fixed)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_valeur = fixed.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_valeur);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_valeur = raw;
+}
--- CPP_2/ex02/Fixed.hpp
+++ CPP_2/ex02/Fixed.hpp
@@ -3,11 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: blandineberthod <blandineberthod@studen    +#+  +:+       +#+        */
+/*   By: bberthod <bberthod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 14:58:49 by blandineber       #+#    #+#             */
-/*   Updated: 2024/02/16 15:03:59 by blandineber      ###   ########.fr       */
+/*   Updated: 2024/02/20 17:54:06 by bberthod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <cmath>
 
+class Fixed
+{
+	private:
+		int	_valeur;
+		const static int _fracBits = 8;
+	public:
+		Fixed(void);
+		Fixed(const int nombre);
+		Fixed(const float nombre);
+		Fixed(const Fixed& fixed);
+		Fixed& operator=(const Fixed& fixed);
+		~Fixed(void);
+		int	getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+};
+
+	std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+
+#endif
